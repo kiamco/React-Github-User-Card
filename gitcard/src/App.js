6c650279a1232cd7416f4df68ec3d868ca7d1d01
kiamco <kiamcoo@gmail.com>
2019-09-06 16:00:58 -0700

    added router to App.js, need to make othersUserComponent

--- gitcard/src/App.js
+++ gitcard/src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './App.css';
 import Axios from 'axios';
 import UserCard from './Components/userCard.js'
+import Styled from 'styled-components';
+import { Route } from 'react-router-dom';
+
+const Button = Styled.button`
+  background: #333;
+  border: 2px solid #333;
+  margin: 10px;
+  color: white;
+  transition:1s;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  &:hover{
+    background: #6cc644;
+    cursor: pointer;
+  }
+  `
+
 
 class App extends React.Component {
 
@@ -12,9 +28,9 @@ class App extends React.Component {
   }
 
   getGitApiData = (first, second, third) => {
-    Axios.all([Axios.get(first), 
-              Axios.get(second), 
-              Axios.get(third)])
+    Axios.all([Axios.get(first),
+    Axios.get(second),
+    Axios.get(third)])
       .then(Axios.spread((first, second, third) => {
         this.setState({ userInfo: first.data, following: second.data, followers: third.data })
         console.log(this.state);
@@ -23,9 +39,9 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.getGitApiData('https://api.github.com/users/kiamco', 
-                       "https://api.github.com/users/kiamco/following",
-                       'https://api.github.com/users/kiamco/followers');
+    this.getGitApiData('https://api.github.com/users/kiamco',
+      "https://api.github.com/users/kiamco/following",
+      'https://api.github.com/users/kiamco/followers');
   }
 
   render() {
@@ -38,11 +54,14 @@ class App extends React.Component {
           followers={this.state.userInfo.followers}
           following={this.state.following}
           getGitApiData={this.getGitApiData} />
-        <button>Followers</button>
-        <button>Following</button>
+        <div className='button-links'>
+          <Button>Followers</Button>
+          <Button>Following</Button>
+        </div>
+
+        {/* <Route exact to='/followers' component={} /> */}
       </div>
 
-      
     )
   }
 }
--- gitcard/src/Components/userCard.js
+++ gitcard/src/Components/userCard.js
@@ -2,21 +2,35 @@ import React from 'react';
 import Styled from 'styled-components';
 
 const CardContainer = Styled.div`
-    max-width:200px;
+    background: #333;
+    margin:10px auto;
+    display:flex;
+    flex-flow: row wrap;
 `
 const Img = Styled.img`
     max-width:200px;
-    border-radius:10rem;
+    height:200px;
+`
+
+const UserDetails = Styled.div`
+    color:white;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    line-height:2;
+    margin:30px;
 `
 class UserCard extends React.Component {
 
     render() {
         return (
             <CardContainer>
-                <Img src={this.props.avatar} alt="avatar" />
-                <h1>Name: {this.props.name}</h1>
-                <p>Followers: {this.props.followers}</p>
-                <p>Number of Repos: {this.props.repos}</p>
+                <div>
+                    <Img src={this.props.avatar} alt="avatar" />
+                </div>
+                <UserDetails>
+                    <h1>Name: {this.props.name}</h1>
+                    <p>Followers: {this.props.followers}</p>
+                    <p>Number of Repos: {this.props.repos}</p>
+                </UserDetails>
             </CardContainer>
         )
     }
