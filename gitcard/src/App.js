4767530abef83652bf1f285fda4812b76f9e8257
kiamco <kiamcoo@gmail.com>
2019-09-06 14:53:23 -0700

    calling 3 apis, addded buttons

--- gitcard/src/App.js
+++ gitcard/src/App.js
@@ -3,33 +3,46 @@ import './App.css';
 import Axios from 'axios';
 import UserCard from './Components/userCard.js'
 
-class App extends React.Component{
+class App extends React.Component {
 
   state = {
-    userInfo: []
+    userInfo: [],
+    following: [],
+    followers: []
   }
 
-  getGitApiData = (api) => {
-    Axios.get(api)
-    .then(res => {
-      this.setState({userInfo: res.data})
-      console.log(this.state)
-    })
-    .catch(err => err)
+  getGitApiData = (first, second, third) => {
+    Axios.all([Axios.get(first), 
+              Axios.get(second), 
+              Axios.get(third)])
+      .then(Axios.spread((first, second, third) => {
+        this.setState({ userInfo: first.data, following: second.data, followers: third.data })
+        console.log(this.state);
+      }))
+
   }
 
-  componentDidMount(){
-    this.getGitApiData('https://api.github.com/users/kiamco');
+  componentDidMount() {
+    this.getGitApiData('https://api.github.com/users/kiamco', 
+                       "https://api.github.com/users/kiamco/following",
+                       'https://api.github.com/users/kiamco/followers');
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className='App'>
-        <UserCard 
+        <UserCard
           avatar={this.state.userInfo.avatar_url}
           name={this.state.userInfo.name}
+          repos={this.state.userInfo.public_repos}
+          followers={this.state.userInfo.followers}
+          following={this.state.following}
           getGitApiData={this.getGitApiData} />
+        <button>Followers</button>
+        <button>Following</button>
       </div>
+
+      
     )
   }
 }
--- gitcard/src/Components/userCard.js
+++ gitcard/src/Components/userCard.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Styled from 'styled-components';
 
+const CardContainer = Styled.div`
+    max-width:200px;
+`
+const Img = Styled.img`
+    max-width:200px;
+    border-radius:10rem;
+`
 class UserCard extends React.Component {
 
     render() {
-        return(
-        <h1>Name: {this.props.name}</h1>
+        return (
+            <CardContainer>
+                <Img src={this.props.avatar} alt="avatar" />
+                <h1>Name: {this.props.name}</h1>
+                <p>Followers: {this.props.followers}</p>
+                <p>Number of Repos: {this.props.repos}</p>
+            </CardContainer>
         )
     }
 }
