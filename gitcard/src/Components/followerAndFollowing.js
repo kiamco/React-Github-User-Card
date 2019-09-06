4c7c9a666069307ddf5d6d42fbb0d0df868334e0
kiamco <kiamcoo@gmail.com>
2019-09-06 16:19:39 -0700

    started otherCards component

---
+++ gitcard/src/Components/followerAndFollowing.js
@@ -0,0 +1,18 @@
+import React from React;
+
+class OtherCards extends React.Components{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default OtherCards;
